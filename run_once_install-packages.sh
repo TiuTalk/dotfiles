afef79dbd46cd4ae8ce8eb57e01022df2ee64cbd
Thiago Belem <github@thiagobelem.net>
2022-08-27 14:09:57 -0300

    Add dot_config/executable_starship.toml Add run_once_0-update-mirrorlist.sh.tmpl Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Update pacman mirrorlist
-# reflector --verbose -c BR -c CL -c PY -c US --protocol https --sort rate --latest 10 --download-timeout 5
-
 # Install packages
 yay -S --needed neovim alacritty ripgrep asdf-vm starship antibody-bin
 
