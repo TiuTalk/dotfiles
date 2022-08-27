9af155d600aa9a4e23a99720058b5333a89148b5
Thiago Belem <github@thiagobelem.net>
2022-08-27 14:43:55 -0300

    Update git config

--- .chezmoiscripts/run_once_install-packages.sh
+++ .chezmoiscripts/run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install packages
-yay -S --needed neovim alacritty tmux tmux-plugin-manager ripgrep asdf-vm starship antibody-bin z
+yay -S --needed neovim alacritty tmux ripgrep asdf-vm starship antibody-bin z diff-so-fancy
 
 # Change shell
 [[ "$SHELL" != "$(which zsh)" ]] && chsh -s $(which zsh)
