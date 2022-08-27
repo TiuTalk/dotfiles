3f6963d27724003047f61e3d11014814948967d3
Thiago Belem <github@thiagobelem.net>
2022-08-27 14:55:47 -0300

    Update scripts

---
+++ .chezmoiscripts/run_once_1-install-packages.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+PACMAN_PACKAGES=(
+  alacritty
+  tmux
+  neovim
+  ripgrep
+  starship
+  z
+  diff-so-fancy 
+)
+
+AUR_PACKAGES=(
+  1password
+  antibody-bin
+  asdf-vm
+  heroku-cli-bin
+  nerd-fonts-fira-code
+  slack-desktop
+  spotify
+  whatsapp-nativefier
+  test1
+)
+
+INSTALLED=$(pacman -Qqme)
+TO_INSTALL=()
+
+for package in ${AUR_PACKAGES[@]}; do
+  [[ "$INSTALLED" =~ "$package" ]] || TO_INSTALL+=($package)
+done
+
+# Install yay packages
+yay -S --needed "${PACMAN_PACKAGES[@]}"
+[ ${#TO_INSTALL[@]} -eq 0 ] && yay -S "${TO_INSTALL[@]}"
+
+# Intsall tmux scripts
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# Change shell
+[[ "$SHELL" != "$(which zsh)" ]] && chsh -s $(which zsh)
+
+true
--- .chezmoiscripts/run_once_install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install packages
-yay -S --needed neovim alacritty tmux ripgrep asdf-vm starship antibody-bin z diff-so-fancy
-
-# Change shell
-[[ "$SHELL" != "$(which zsh)" ]] && chsh -s $(which zsh)
-
-true
