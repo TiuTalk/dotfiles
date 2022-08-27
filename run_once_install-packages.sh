2e785cf43070cf47c49e4e75a63a38cb66638ef2
Thiago Belem <github@thiagobelem.net>
2022-08-27 13:52:22 -0300

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-{{- $reflector = promptBool "Update Pacman mirror list?" -}}
-
 # Update pacman mirrorlist
-[[ {{$reflector}} ]] && reflector --verbose -c BR -c CL -c PY -c US --protocol https --sort rate --latest 10 --download-timeout 5
+# reflector --verbose -c BR -c CL -c PY -c US --protocol https --sort rate --latest 10 --download-timeout 5
 
 # Install packages
 yay -S --needed neovim alacritty ripgrep asdf-vm starship antibody-bin
